254ba2f262bc34196a40eb2c949f7e2c60bb9049
Venkata Sai Rahul Unnam <52215587+uvsrahul1234@users.noreply.github.com>
2026-07-12 16:21:39 -0500

    training pipleine

---
+++ run_training_pipeline.py
@@ -0,0 +1,22 @@
+import logging
+
+from steps.import_data import import_data
+from steps.train_model import train_model
+from steps.evaluate_model import evaluate_model
+from pipelines.training_pipeline import train_pipeline
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def run_training():
+    training = train_pipeline(
+        import_data(),
+        train_model(),
+        evaluate_model(),
+    )
+
+    training.run()
+
+
+if __name__ == "__main__":
+    run_training()
